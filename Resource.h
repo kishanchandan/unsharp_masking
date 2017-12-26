42ef878a8de845d38de6492ac0d839b827475090
kishanchandan <34803129+kishanchandan@users.noreply.github.com>
2017-12-25 21:02:00 -0500

    Unsharp Masking C++ Visual Studio
    Just Import these files in your Visual Studio Project. The file UnsharpMasking.cpp is the core of the code and contains function for Unsharp Masking.

---
+++ Resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by kingimage.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_KINGIMTYPE                  129
+#define ID_PROCESS                      32771
+#define ID_any                          32772
+#define ID_SOBELOPERATOR                32773
+#define ID_SOBEL                        32774
+#define ID_UNSHARPMASKING               32775
+#define ID_USHARP                       32776
+#define ID_UNSHARPMASKING32777          32777
+#define ID_USHARPMASKING                32778
+#define ID_LOGMASK7                     32779
+#define ID_LOGMASK11                    32780
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
